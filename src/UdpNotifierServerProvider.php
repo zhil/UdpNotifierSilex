36d489248ff06b3624751654b7ec36de1b6e79a8
zhil <starsfm@gmail.com>
2016-05-05 17:02:52 +0300

    Update UdpNotifierServerProvider.php
    added custom port support

--- src/UdpNotifierServerProvider.php
+++ src/UdpNotifierServerProvider.php
@@ -9,10 +9,10 @@ class UdpNotifierServerProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        $app['zhil.udp_notifier.server'] = $app->protect(function ($handler,$banHandler) use ($app) {
+        $app['zhil.udp_notifier.server'] = $app->protect(function ($handler,$banHandler,$customPort=null) use ($app) {
             $listener = new \Zhil\UdpNotifier\Listener(
                 $app['zhil.udp_notifier.options']["ip"],
-                $app['zhil.udp_notifier.options']["port"],
+                $customPort?$customPort:$app['zhil.udp_notifier.options']["port"],
                 $app['zhil.udp_notifier.options']["secret"]);
             $listener->run(function($message, $address) use ($app,$handler) {
                 if($json = json_decode($message)) {
